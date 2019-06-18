ece721ef2a5bd8c64c7f5c19b7adc993067eb087
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2019-06-18 12:36:09 +0430

    Deploy to add-design-elements-to-reports

--- br_add-design-elements-to-reports/js/binary.js
+++ br_add-design-elements-to-reports/js/binary.js
@@ -32371,7 +32371,7 @@ var binary_desktop_app_id = 14473;
 
 var getAppId = function getAppId() {
     var app_id = null;
-    var user_app_id = '17037'; // you can insert Application ID of your registered application here
+    var user_app_id = '17036'; // you can insert Application ID of your registered application here
     var config_app_id = window.localStorage.getItem('config.app_id');
     if (config_app_id) {
         app_id = config_app_id;
